fa146aecacf6cf53b4c36eba5ec9eb66d6b30556
veeresh-javali18 <veereshjavali038@gmail.com>
2025-04-24 10:57:41 +0530

    saved

---
+++ Demi.java
@@ -0,0 +1,10 @@
+class Demo {
+    public static void main(String[] args) {
+        int a=10;
+        int b=20;
+        int sum=a+b;
+        int sub=a-b;
+        System.out.println("Sum is :"+sum);
+        System.out.println("Sub is :"+sub);
+    }
+}
