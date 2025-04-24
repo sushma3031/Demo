34680f686aef97da93be01e8c8e22ee6afd8cffe
sushma3031 <sushmabhadragoudra@gmail.com>
2025-04-24 11:11:45 +0530

    saved

---
+++ Demo.java
@@ -0,0 +1,14 @@
+public class Demo {
+    
+        public static void main(String[] args) {
+            int a=10;
+            int b=20;
+            int sum=a+b;
+            int sub=a-b;
+            System.out.println("Sum is :"+sum);
+            System.out.println("Sub is :"+sub);
+        }
+    }
+
+
+
